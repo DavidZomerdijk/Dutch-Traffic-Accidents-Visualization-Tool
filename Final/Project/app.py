a1184b9ad3cf945f04b9fa484d9d6cbd762d3bec
Unknown <dylanwijman@hotmail.com>
2017-03-21 16:14:49 +0100

    Added weather conditions

--- Final/Project/app.py
+++ Final/Project/app.py
@@ -34,7 +34,8 @@ def data(year= 2015 ):
     #here we add the population data
     outputDict = {}
     for key, value in json.loads(output).items():
-        outputDict[key] = {"accidents": value, "per_capita": round((int(value) / populationData[key] ), 5)}
+        weather = result[(result["PVE_NAAM"] == key)]["WGD_CODE_1"]
+        outputDict[key] = {"weather":weather.value_counts().to_json(orient="columns"), "accidents": value, "per_capita": round((int(value) / populationData[key] ), 5)}
 
     return json.dumps(outputDict)
 
