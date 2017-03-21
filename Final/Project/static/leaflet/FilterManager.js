41ff2db6d853ee69c527bb21646a47ed7acf4bef
Julian Hana <julian_hana@hotmail.com>
2017-03-21 14:48:08 +0100

    Created a class for the toggles, can now be linked with data

---
+++ Final/Project/static/leaflet/FilterManager.js
@@ -0,0 +1,73 @@
+/**
+ * Created by julianhana on 21/03/17.
+ */
+$(function() {
+    $('#male').change(function() {
+        console.log("male toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+
+$(function() {
+    $('#female').change(function() {
+        console.log("female toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+
+
+$(function() {
+    $('#age1').change(function() {
+        console.log("age 1 toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+
+$(function() {
+    $('#age2').change(function() {
+        console.log("age 2 toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+$(function() {
+    $('#age3').change(function() {
+        console.log("age 3 toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+$(function() {
+    $('#age4').change(function() {
+        console.log("age 4 toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+$(function() {
+    $('#storm').change(function() {
+        console.log("storm toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+$(function() {
+    $('#rain').change(function() {
+        console.log("rain toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+$(function() {
+    $('#fog').change(function() {
+        console.log("fog toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+$(function() {
+    $('#snow').change(function() {
+        console.log("snow toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
+$(function() {
+    $('#sunny').change(function() {
+        console.log("sunny toggled");
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+    })
+})
