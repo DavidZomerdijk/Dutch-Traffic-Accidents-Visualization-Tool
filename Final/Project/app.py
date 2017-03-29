ff9acf7278bf46da5991b52400fd6cc5558f2251
davidzomerdijk <david.zomerdijk93@gmail.com>
2017-03-29 17:19:32 +0200

    feeling tired

--- Final/Project/app.py
+++ Final/Project/app.py
@@ -14,8 +14,6 @@ import pickle
 PATH_TO_DATA = "../../Data/verkeersOngelukkenNederland.p"
 app = flask.Flask(__name__)
 
-max_color_value = 0.0
-min_color_value = 1000.0
 
 #######################################
 # this is for the main map
@@ -28,14 +26,14 @@ def index():
     return flask.render_template("home.html")
 
 @app.route("/data")
-@app.route("/data/<int:year>")
-@app.route("/data/<int:year>/<weer>")
-def data(year= 2015 , weer = "all" ):
+@app.route("/data/<int:year>/<int:minTijd>/<int:maxTijd>/<weer>")
+def data(year= 2015,minTijd=0, maxTijd=24, weer='all'  ):
     global max_color_value
     global min_color_value
     max_color_value = 0.0
     min_color_value = 1000.0
-    provinces = ["Noord-Holland", "Zuid-Holland", "Groningen", "Friesland", "Drenthe", "Overijssel", "Flevoland", "Gelderland", "Utrecht", "Zeeland", "Noord-Brabant", "Limburg"]
+    provinces = ["Noord-Holland", "Zuid-Holland", "Groningen", "Friesland", "Drenthe", "Overijssel", "Flevoland",
+                 "Gelderland", "Utrecht", "Zeeland", "Noord-Brabant", "Limburg"]
     if weer == "all":
         result = accidentData[ accidentData["JAAR_VKL"] == year   ]
     else:
@@ -49,7 +47,7 @@ def data(year= 2015 , weer = "all" ):
         outputDict[key] = {"accidents": value, "per_capita": round((int(value) / populationData[key] ), 5)}
         provinces.remove(key)
         #   weather = result[(result["PVE_NAAM"] == key)]["WGD_CODE_1"]
-        #"weather":weather.value_counts().to_json(orient="columns"),
+        # "weather":weather.value_counts().to_json(orient="columns"),
         if max_color_value < round((int(value) / populationData[key]), 5):
             max_color_value = round((int(value) / populationData[key]), 5)
         if min_color_value > round((int(value) / populationData[key]), 5):
@@ -63,7 +61,8 @@ def data(year= 2015 , weer = "all" ):
 def maxvalue():
     global max_color_value
     global min_color_value
-    return flask.jsonify({"max_value"  : str(max_color_value), "min_value" : str(min_color_value)})
+    return flask.jsonify({"max_value": str(max_color_value), "min_value": str(min_color_value)})
+
 
 #######################################
 # this is for the pointMap
@@ -109,23 +108,22 @@ def getProvinceBounds():
 
 
 @app.route("/dataCoordinates")
-@app.route("/dataCoordinates/<int:year>/<int:minTijd>/<int:maxTijd>")
-def dataCoordinates(year= 2015,minTijd=0, maxTijd=24 ):
-    #filter on year
-    data_filtered = accidentData[ (accidentData["JAAR_VKL"] == year)]
+@app.route("/dataCoordinates/<int:year>/<int:minTijd>/<int:maxTijd>/<weer>")
+def dataCoordinates(year= 2015,minTijd=0, maxTijd=24, weer='all' ):
+    global data_filtered
+    if weer == "all":
+        data_filtered = accidentData[ accidentData["JAAR_VKL"] == year   ]
+    else:
+        data_filtered = accidentData[ accidentData["JAAR_VKL"] == year   ][accidentData["WGD_CODE_1"] == str(weer)]
 
     #filter on province
-    output = data_filtered[data_filtered["PVE_NAAM"] == currentProvince]
+    data_filtered = data_filtered[data_filtered["PVE_NAAM"] == currentProvince]
 
     #filter on time
-    output = output[ (output["UUR"] >= minTijd) & (output["UUR"] <= maxTijd) ]
-
-    #only take lat lon
-    output = output[['lat', 'lon']]
+    data_filtered = data_filtered[ (data_filtered["UUR"] >= minTijd) & (data_filtered["UUR"] <= maxTijd) ]
 
     #group and sum
-    output = output[['lat', 'lon']].groupby(['lat', 'lon']).size().reset_index(name="accidents")
-
+    output = data_filtered[['lat', 'lon']].groupby(['lat', 'lon']).size().reset_index(name="accidents")
 
     return output.to_json(orient="records")
 
@@ -134,7 +132,7 @@ def dataCoordinates(year= 2015,minTijd=0, maxTijd=24 ):
 @app.route("/dangerousPoints/<int:distance>")
 @app.route("/dangerousPoints/<int:distance>/<int:year>")
 def dangerousPoints(distance=1, year=2015):
-    data_filtered = accidentData[ accidentData["JAAR_VKL"] == year][accidentData["PVE_NAAM"] == currentProvince ]
+    # data_filtered = accidentData[ accidentData["JAAR_VKL"] == year][accidentData["PVE_NAAM"] == currentProvince ]
     return flask.jsonify( {"dangerousPoints" : dangerPoints(data_filtered, distance)})
 
 def dangerPoints(df, d=1):
--- Final/Project/static/data_handler.js
+++ Final/Project/static/data_handler.js
@@ -3,10 +3,10 @@ var minTijd = "0"
 var maxTijd = "24";
 var weer = "all"
 
-d3.select("#slider").on("input", function() {
-    year = String(+this.value);
-    update();
-});
+//d3.select("#slider").on("input", function() {
+//    year = String(+this.value);
+//    update();
+//});
 
 var callback = function (d) {
     provinceData = d
@@ -23,3 +23,74 @@ function update() {
 
 
 update()
+
+//FILTERS
+//standard button for dangerous accidents
+var filterHeader = '<p id="filterHeader">Filters </p>'
+var sliderJaar ='<div id="sliderJaar"></div>'  //'<div id="selectedYear"></div> <input type="range" value=2009 min=2003 max=2015 id="sliderYear" >';
+var sliderJaarHeader = '<p><label for="jaar">Year: </label><input style="width:50px!important" type="text" id="yearValue" readonly style="border"</p>'
+var sliderTijdHeader = '<p><label for="hours">Time range: </label><input  style="width:50px!important" type="text" id="amount" readonly style="border"</p>'
+var sliderTijd = '<div id="slider-range"></div>';
+var dropdownWeer ='<select id ="dropDownWeer" value="this.value" onchange="changeWeer(this.value)">'
+dropdownWeer = dropdownWeer.concat('<option value="all" >all</option>')
+dropdownWeer = dropdownWeer.concat('<option value="D">Dry</option>')
+dropdownWeer = dropdownWeer.concat('<option value="R">Rain</option>')
+dropdownWeer = dropdownWeer.concat('<option value="M">Mist</option>')
+dropdownWeer = dropdownWeer.concat('<option value="S">Snow/Hail</option>')
+dropdownWeer = dropdownWeer.concat('<option value="H">Hard wind</option>')
+dropdownWeer = dropdownWeer.concat('<option value="O">unknown</option></select>')
+var dropDownWeg;
+
+
+var filters = L.control({position:"bottomleft"})
+    filters.onAdd= function(map){
+        var div = L.DomUtil.create('div', 'title')
+        var htmlText = '<div id="filters"> ';
+        htmlText = htmlText.concat( filterHeader )
+        htmlText = htmlText.concat(sliderJaarHeader)
+        htmlText = htmlText.concat( sliderJaar )
+        htmlText = htmlText.concat(sliderTijdHeader)
+        htmlText = htmlText.concat( sliderTijd )
+        htmlText=  htmlText.concat('<br><div dropDownWeerHeader>Weather condition: </div>')
+        htmlText = htmlText.concat( dropdownWeer )
+        div.innerHTML = htmlText.concat("</div>");
+        div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
+        console.log(div.innerHTML)
+        return div;
+    }
+
+filters.addTo(map)
+
+
+$( "#sliderJaar" ).slider({
+    min:2009,
+    max:2015,
+    value: 2013,
+    slide: function( event, ui ) {
+        year = ui.value;
+        updatePointData();
+        $( "#yearValue" ).val(  + ui.value);
+    }
+});
+    $( "#yearValue" ).val(  $( "#sliderJaar" ).slider( "value") )
+
+
+$( function() {
+$( "#slider-range" ).slider({
+  range: true,
+  min: 0,
+  max: 24,
+  values: [ 0, 24 ],
+  slide: function( event, ui ) {
+        minTijd = ui.values[0];
+        maxTijd = ui.values[1];
+        updatePointData();
+    $( "#amount" ).val(  + ui.values[ 0 ] + "h -" + ui.values[ 1 ] +"h" );
+  }
+});
+$( "#amount" ).val(  $( "#slider-range" ).slider( "values", 0 ) +
+  "h - " + $( "#slider-range" ).slider( "values", 1 ) + "h");
+} );
+
+
+
