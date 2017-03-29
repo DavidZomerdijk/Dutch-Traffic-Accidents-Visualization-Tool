cd7dfd65a836bbd8b5a4ce29aca92edb1f0200fc
davidzomerdijk <david.zomerdijk93@gmail.com>
2017-03-29 17:08:37 +0200

    pointMap finished

--- Final/Project/static/data_handler.js
+++ Final/Project/static/data_handler.js
@@ -1,42 +1,25 @@
-var weathercondition = "all";
-var selected_year = 2009;
+var year = "2013";
+var minTijd = "0"
+var maxTijd = "24";
+var weer = "all"
 
 d3.select("#slider").on("input", function() {
-    selected_year = String(+this.value);
-    update( String(+this.value));
-    updateweer(weathercondition);
+    year = String(+this.value);
+    update();
 });
 
-function weather(value)
-{
-    weathercondition = value;
-    updateWeer();
-};
-
-// Show the information about a particular point.
-var show_info = function (d) {
-    d3.select("#info").text( d );
-};
-
 var callback = function (d) {
-    //d3.select("#info").text( JSON.stringify(d, null, 2) );
-    //here we define the data variable
     provinceData = d
     update_map()
     updateChart()
-
+    console.log("provinceData is defined as:")
+    console.log( provinceData )
 }
 
 // Load the data.
-function update(year) {
-    d3.select("#selectedYear").text( year );
-    d3.json("/data/" + String(year), callback)
+function update() {
+    d3.json("/data/" + String(year) + "/" + String(minTijd) + "/" +String( maxTijd ) +"/" +String(weer), callback)
 };
 
-function updateWeer() {
-    console.log("/data/" + selected_year + "/" + String(weathercondition) )
-    d3.json("/data/" + selected_year + "/" + String(weathercondition), callback);
-        //underneath we update the map using a function from show_map
-    }
 
-updateWeer();
+update()
